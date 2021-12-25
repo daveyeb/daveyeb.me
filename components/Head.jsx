16735996f044d79696e8e3d42ec0919054f71388
David Yeboah <daveyeb@gmail.com>
2021-12-25 16:41:46 -0500

    Starting over

---
+++ components/Head.jsx
@@ -0,0 +1,26 @@
+import {
+    default as NHead
+} from 'next/head'
+
+export default ({ title, desc }) => (
+    <NHead>
+        <title>
+            {title}
+        </title>
+
+        <meta charSet="utf-8" />
+
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta content={title} name="title" />
+        <meta content={title} property="og:title" />
+        <meta content={title} name="twitter:title" />
+        <meta content={desc} name="description" />
+        <meta content={desc} property="og:description" />
+        <meta content={desc} name="twitter:description" />
+        <meta content="en" property="og:locale" />
+
+
+        <link rel="stylesheet" href="https://use.typekit.net/jlh4gie.css"></link>
+
+    </NHead>
+);
---
+++ components/Nav.jsx
@@ -0,0 +1,12 @@
+export default () => (
+    <>
+        <div className="row red short">
+            <div className="column col-xs-12 p1 red">
+                <div className="row red">
+
+                </div>
+            </div>
+        </div>
+    
+    </>
+)
