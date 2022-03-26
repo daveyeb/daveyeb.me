9a908b659033cc3106cac36dad5bb0701517d504
David Yeböah <themoon@Davids-MBP.attlocal.net>
2022-03-26 09:36:17 -0400

    moving work to desktop

--- pages/components/Nav.jsx
+++ pages/components/Nav.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
+
 const Nav = () => {
+    var [clicked, setClicked] = useState()
+
+    console.log(clicked)
+
     return (
         <>
             <div className="col-sm-1 nav p-3">
                 <span>home</span>
                 <div className="d-flex flex-column justify-content-end nav-items pb-3">
-                    <div className="rel pos1"><a href="">blog</a></div>
-                    <div className="rel pos2"><a href="">projects</a></div>
+                    <div className="rel pos1">
+                        {
+                            !clicked ? 
+                            <a href="" className={clicked ? 'next' : ''} onClick={ () => setClicked(!clicked)} >blog</a>
+                            : <a href="" className={clicked ? 'prev' : ''} >projects</a>
+                        }
+                    </div>
+                    {/* <div className="rel pos2"><a href="" className={clicked ? 'next' : ''} >projects</a></div> */}
                     <div className="rel pos3"><a href="">playlists</a></div>
                 </div>
             </div>
