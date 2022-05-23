eaed9bcdd4b839f482bf56f3b70a81e09be3d58e
David Yeböah <themoon@Davids-MBP.attlocal.net>
2022-05-22 23:42:48 -0400

    refined nav

---
+++ pages/components/DigitalLink.jsx
@@ -0,0 +1,80 @@
+import { useContext, useEffect, useState } from "react";
+import { NavContext } from "./NavContext";
+import { append } from "./util";
+
+export const DigitalLink = ({ children, index }) => {
+  const [links, setLinks, meta, updateMeta, disabled, setDisabled] = useContext(NavContext);
+  const [loading, setLoading] = useState(true);
+
+  const [curr, setCurr] = useState("");
+  const [next, setNext] = useState("");
+
+  var getNext = () => {
+    if(disabled) return
+
+    setDisabled(true);
+    setNext(meta.active);
+    updateMeta(Object.assign(meta, { active: curr }));
+  };
+
+  var reset = () => {
+    setCurr(next);
+    setNext("");
+    setDisabled(false);
+  };
+
+  useEffect(() => {
+    setLinks(append(links, children));
+    updateMeta(Object.assign(meta, { links }));
+    
+    if (curr === "") setCurr(links[index]);
+
+    setLoading(false);
+  }, [links, curr, next]);
+
+  return loading ? (
+    <>still lsoading</>
+  ) : (
+    <>
+      <div className="row position-relative overflow-hidden">
+        <a
+          onClick={getNext}
+          onAnimationEnd={reset}
+          className={`${next !== "" ? "prev position-absolute" : ""}`}
+        >
+          <span className={`dotted ${disabled ? '' : 'pointer'}`}>{curr}</span>
+        </a>
+        <a className={`${next !== "" ? "next" : "d-none"}`}>{next}</a>
+      </div>
+      <style jsx>{`
+        a {
+          text-decoration: none !important;
+          color: rgba(0,0,0,.6);
+          text-rendering: optimizeLegibility;
+          cursor: default;
+        }
+
+        .pointer {
+          cursor: pointer;
+        }
+
+        .dotted {
+          border-bottom: 1px dotted #9b9b9b;
+        }
+
+        .dotted:hover {
+          border-bottom: none;
+          color: #0021e5 !important;
+        }
+
+        .next {
+          animation: up 0.5s forwards;
+        }
+
+        .prev {
+          animation: p___up 0.5s forwards;
+        }
+      `}</style>
+    </>
+  );
+};
--- pages/components/Nav.jsx
+++ pages/components/Nav.jsx
@@ -1,56 +1,23 @@
 import { useState } from "react";
-import { DigiLink } from "./DigiLink";
+import { DigitalLink } from "./DigitalLink";
 import { NavContext } from "./NavContext";
 
-
-
 export const Nav = () => {
-  var [pages, setPages] = useState({ active: 'home', pages: ['projects', 'blog', 'playlists'] });
+  const [meta, updateMeta] = useState({active: "playlists"});
+  const [links, setLinks] = useState([]);
   const [disabled, setDisabled] = useState(false);
 
   return (
     <>
-      <NavContext.Provider value={[pages, setPages, disabled, setDisabled]}>
-        <div className="m-1 row p-3 nav">
-          <div className="display-6">
-          </div>
-          <div className="align-self-end">
-            {pages.pages.map((page, i) => (
-              <DigiLink
-                key={i}
-                page={page}
-                onPress={() => pages.active}
-              ></DigiLink>
-            ))}
+      <NavContext.Provider value={[links, setLinks, meta, updateMeta, disabled, setDisabled]}>
+        <div className=" nav col-2 col">
+          <div className="align-self-end p-3">
+            <DigitalLink index={0}>home</DigitalLink>
+            <DigitalLink index={1}>blog</DigitalLink>
+            <DigitalLink index={2}>projects</DigitalLink>
           </div>
         </div>
       </NavContext.Provider>
     </>
   );
 };
-
-
-
-// https://w3.ibm.com/#/
-
-// -- home page
-
-// https://w3.ibm.com/#/people/039078897
-
-// -- corporate page
-
-// https://ibm.webex.com/webappng/sites/ibm/dashboard/pmr/james.kurkela1
-
-// -- cross meeting 
-
-// https://w3.ibm.com/youandibm/us/topics
-// https://w3.ibm.com/#/apps/96e42fa3-5f81-4a97-86a2-978f48484158
-
-// -- benefits
-
-// mount sapo site 
-
-
-
-
-
