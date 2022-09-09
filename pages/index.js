1aa3e6287050693cdf92e08086fc95995019f95a
David Yeböah <themoon@dhcp-9-240-34-83.k1v-ca.ibmmobiledemo.com>
2022-09-09 13:36:18 -0400

    added my custom favicon

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,14 @@ export default function Home() {
       <Head>
         <title>david yebōah , swe | home</title>
         <meta name="description" content="personal website of david.yeboah" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="favicon/site.webmanifest"/>
+        <meta name="msapplication-config" content="favicon/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
 
       <main className="">
